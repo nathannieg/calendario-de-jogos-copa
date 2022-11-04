d6596eab5f93fbcc922e51ac02083ed1b25742e7
Nathannie Gomes <nathannieg@gmail.com>
2022-11-04 18:39:58 -0300

    adding a list of objects + more matches

---
+++ db.js
@@ -0,0 +1,128 @@
+export const matches = [
+  {
+    date: "21/11",
+    day: "segunda",
+    player1: "england",
+    time: "10:00",
+    player2: "iran"
+  },
+  {
+    date: "21/11",
+    day: "segunda",
+    player1: "senegal",
+    time: "13:00",
+    player2: "netherlands"
+  },
+  {
+    date: "22/11",
+    day: "terça",
+    player1: "argentina",
+    time: "07:00",
+    player2: "saudi arabia"
+  },
+  {
+    date: "22/11",
+    day: "terça",
+    player1: "france",
+    time: "16:00",
+    player2: "australia"
+  },
+  {
+    date: "23/11",
+    day: "quarta",
+    player1: "germany",
+    time: "10:00",
+    player2: "japan"
+  },
+  {
+    date: "24/11",
+    day: "quinta",
+    player1: "brazil",
+    time: "16:00",
+    player2: "serbia"
+  },
+  {
+    date: "25/11",
+    day: "sexta",
+    player1: "netherlands",
+    time: "13:00",
+    player2: "ecuador"
+  },
+  {
+    date: "25/11",
+    day: "sexta",
+    player1: "england",
+    time: "16:00",
+    player2: "united states"
+  },
+  {
+    date: "26/11",
+    day: "sábado",
+    player1: "france",
+    time: "13:00",
+    player2: "denmark"
+  },
+  {
+    date: "27/11",
+    day: "domingo",
+    player1: "spain",
+    time: "16:00",
+    player2: "germany"
+  },
+  {
+    date: "28/11",
+    day: "segunda",
+    player1: "brazil",
+    time: "13:00",
+    player2: "switzerland"
+  },
+  {
+    date: "28/11",
+    day: "segunda",
+    player1: "portugal",
+    time: "16:00",
+    player2: "uruguay"
+  },
+  {
+    date: "29/11",
+    day: "terça",
+    player1: "netherlands",
+    time: "12:00",
+    player2: "qatar"
+  },
+  {
+    date: "29/11",
+    day: "terça",
+    player1: "wales",
+    time: "16:00",
+    player2: "england"
+  },
+  {
+    date: "30/11",
+    day: "quarta",
+    player1: "france",
+    time: "12:00",
+    player2: "tunisia"
+  },
+  {
+    date: "01/12",
+    day: "quinta",
+    player1: "costa rica",
+    time: "16:00",
+    player2: "germany"
+  },
+  {
+    date: "02/12",
+    day: "sexta",
+    player1: "ghana",
+    time: "12:00",
+    player2: "uruguay"
+  },
+  {
+    date: "02/12",
+    day: "sexta",
+    player1: "cameroon",
+    time: "16:00",
+    player2: "brazil"
+  }
+]
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { matches } from "./db.js"
+
 // Creates game
 function createGame(player1, time, player2) {
   return `
@@ -29,13 +31,82 @@ function createCard(date, day, game) {
   `
 }
 
-// Creating the whole HTML structure via JS
 document.querySelector("#cards").innerHTML =
+  // 21/11
   createCard(
-    "24/11",
-    "quinta",
-    createGame("uruguay", "10:00", "south korea") +
-      createGame("brazil", "16:00", "serbia")
+    matches[0].date,
+    matches[0].day,
+    createGame(matches[0].player1, matches[0].time, matches[0].player2) +
+      createGame(matches[1].player1, matches[1].time, matches[1].player2)
   ) +
-  createCard("28/11", "segunda", createGame("brazil", "13:00", "switzerland")) +
-  createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
+  // 22/11
+  createCard(
+    matches[2].date,
+    matches[2].day,
+    createGame(matches[2].player1, matches[2].time, matches[2].player2) +
+      createGame(matches[3].player1, matches[3].time, matches[3].player2)
+  ) +
+  // 23/11
+  createCard(
+    matches[4].date,
+    matches[4].day,
+    createGame(matches[4].player1, matches[4].time, matches[4].player2)
+  ) +
+  // 24/11
+  createCard(
+    matches[5].date,
+    matches[5].day,
+    createGame(matches[5].player1, matches[5].time, matches[5].player2)
+  ) +
+  // 25/11
+  createCard(
+    matches[6].date,
+    matches[6].day,
+    createGame(matches[6].player1, matches[6].time, matches[6].player2) +
+      createGame(matches[7].player1, matches[7].time, matches[7].player2)
+  ) +
+  // 26/11
+  createCard(
+    matches[8].date,
+    matches[8].day,
+    createGame(matches[8].player1, matches[8].time, matches[8].player2)
+  ) +
+  // 27/11
+  createCard(
+    matches[9].date,
+    matches[9].day,
+    createGame(matches[9].player1, matches[9].time, matches[9].player2)
+  ) +
+  // 28/11
+  createCard(
+    matches[10].date,
+    matches[10].day,
+    createGame(matches[10].player1, matches[10].time, matches[10].player2) +
+      createGame(matches[11].player1, matches[11].time, matches[11].player2)
+  ) +
+  // 29/11
+  createCard(
+    matches[12].date,
+    matches[12].day,
+    createGame(matches[12].player1, matches[12].time, matches[12].player2) +
+      createGame(matches[13].player1, matches[13].time, matches[13].player2)
+  ) +
+  // 30/11
+  createCard(
+    matches[14].date,
+    matches[14].day,
+    createGame(matches[14].player1, matches[14].time, matches[14].player2)
+  ) +
+  // 01/12
+  createCard(
+    matches[15].date,
+    matches[15].day,
+    createGame(matches[15].player1, matches[15].time, matches[15].player2)
+  ) +
+  // 02/12
+  createCard(
+    matches[16].date,
+    matches[16].day,
+    createGame(matches[16].player1, matches[16].time, matches[16].player2) +
+      createGame(matches[17].player1, matches[17].time, matches[17].player2)
+  )
